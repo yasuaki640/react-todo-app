b96b52d618539cb6a271e77a893293116e976428
yasuaki640 <yasuaki640@gmail.com>
2020-12-10 21:07:25 +0900

    add template html

--- src/App.js
+++ src/App.js
@@ -3,9 +3,39 @@ import "./styles.css";
 
 export default function App() {
     return (
-        <div className="App">
-            <h1>Hello CodeSandbox</h1>
-            <h2>Start editing to see some magic happen!</h2>
-        </div>
+        <>
+            <div>
+                <input placeholder={"input TODO"}/>
+                <button>add</button>
+            </div>
+            <div>
+                <p>UNDONE</p>
+                <ul>
+                    <div>
+                        <li>三の倍数だけ</li>
+                        <button>complete</button>
+                        <button>delete</button>
+                    </div>
+                    <div>
+                        <li>三の倍数だけ</li>
+                        <button>complete</button>
+                        <button>delete</button>
+                    </div>
+                </ul>
+            </div>
+            <div>
+                <p>DONE</p>
+                <ul>
+                    <div>
+                        <li>あほになる</li>
+                        <button>return</button>
+                    </div>
+                    <div>
+                        <li>あほになる</li>
+                        <button>return</button>
+                    </div>
+                </ul>
+            </div>
+        </>
     );
 }
